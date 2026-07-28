05c66a7112e11e44e5b58400c2fc99f02d513e8a
MilaBikar <milabikar@gmail.com>
2026-07-28 17:18:24 +0200

    Added AutoMapper

---
+++ SprintService/Profiles/SprintProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using SprintService.Models;
+using SprintService.Models.DTO;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace SprintService.Profiles
+{
+    public class SprintProfile : Profile
+    {
+        public SprintProfile()
+        {
+            CreateMap<SprintCreationDTO, Sprint>();
+
+            CreateMap<Sprint, SprintDTO>();
+
+            CreateMap<SprintUpdateDTO, Sprint>();
+
+            CreateMap<Sprint, SprintConfirmationDTO>();
+        }
+    }
+}
--- SprintService/Program.cs
+++ SprintService/Program.cs
@@ -1,3 +1,7 @@
+using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using SprintService.Profiles;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAutoMapper(config => config.AddMaps(typeof(Program).Assembly));
 
 var app = builder.Build();
 
