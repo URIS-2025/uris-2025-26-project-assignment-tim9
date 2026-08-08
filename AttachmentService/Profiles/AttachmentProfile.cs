ae454d09e934652033e927e0298d346d5f4cd28d
MilaBikar <milabikar@gmail.com>
2026-08-08 15:48:18 +0200

    Added AutoMapper

---
+++ AttachmentService/Profiles/AttachmentProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using AttachmentService.Models;
+using AttachmentService.Models.DTO;
+
+namespace AttachmentService.Profiles
+{
+    public class AttachmentProfile : Profile
+    {
+        public AttachmentProfile()
+        {
+            CreateMap<AttachmentCreationDTO, Attachment>();
+
+            CreateMap<Attachment, AttachmentDTO>();
+
+            CreateMap<AttachmentUpdateDTO, Attachment>();
+
+            CreateMap<Attachment, AttachmentConfirmationDTO>();
+        }
+    }
+}
