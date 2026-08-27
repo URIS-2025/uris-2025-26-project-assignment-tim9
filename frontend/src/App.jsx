c981bdb77f7deafa2bb26ac8cad04c34fb74f3c8
SaraJovic <sjovic650@gmail.com>
2026-08-27 17:52:36 +0200

    Add components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,17 @@
 import WorkPackageList from './features/workpackage/components/WorkPackageList'
 import TaskBoard from './features/workpackage/components/TaskBoard'
+import SubTaskTree from './features/workpackage/components/SubTaskTree'
+import TaskDetails from './features/workpackage/components/TaskDetails'
+import BacklogView from './features/workpackage/components/BacklogView'
 
 function App() {
   return (
     <div>
       <WorkPackageList projectId="00000000-0000-0000-0000-000000000000" />
       <TaskBoard workPackageId="00000000-0000-0000-0000-000000000000" />
+      <SubTaskTree taskId="00000000-0000-0000-0000-000000000000" />
+      <TaskDetails taskId="00000000-0000-0000-0000-000000000000" />
+      <BacklogView projectId="00000000-0000-0000-0000-000000000000" />
     </div>
   )
 }
