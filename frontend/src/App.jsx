ef285aabf679806158e55a1a419afde518e6880d
Neda J <nedaajanjicic@gmail.com>
2026-08-29 17:36:45 +0200

    Povezi Login stranicu u App.jsx, ukloni link ka Sign Up-u
    AuthProvider sad omotava celu aplikaciju, /login ruta je dodata, a
    Projects/WorkPackages/Timelogs rute su zasticene sa RequireAuth. Link
    ka /signup je uklonjen iz LoginPage-a jer ta stranica pripada
    feature/frontend-user-service grani, ne ovoj.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import AuthProvider from './auth/AuthProvider'
+import RequireAuth from './auth/RequireAuth'
+import LoginPage from './pages/Auth/LoginPage'
 import ProjectListPage from './pages/Projects/ProjectListPage'
 import ProjectDetailsPage from './pages/Projects/ProjectDetailsPage'
 import WorkPackagesPage from './pages/WorkPackages/WorkPackagePage'
@@ -7,14 +10,53 @@ import TimelogsPage from './pages/Timelogs/TimelogsPage'
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/projects" replace />} />
-        <Route path="/projects" element={<ProjectListPage />} />
-        <Route path="/projects/:id" element={<ProjectDetailsPage />} />
-        <Route path="/projects/:projectId/work-packages" element={<WorkPackagesPage />} />
-        <Route path="/projects/:projectId/timelogs" element={<TimelogsPage />} />
-        <Route path="/timelogs" element={<TimelogsPage />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Navigate to="/projects" replace />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route
+            path="/projects"
+            element={
+              <RequireAuth>
+                <ProjectListPage />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/projects/:id"
+            element={
+              <RequireAuth>
+                <ProjectDetailsPage />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/projects/:projectId/work-packages"
+            element={
+              <RequireAuth>
+                <WorkPackagesPage />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/projects/:projectId/timelogs"
+            element={
+              <RequireAuth>
+                <TimelogsPage />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/timelogs"
+            element={
+              <RequireAuth>
+                <TimelogsPage />
+              </RequireAuth>
+            }
+          />
+          <Route path="*" element={<Navigate to="/projects" replace />} />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   )
 }
